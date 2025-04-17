8069c5adca4b14a4f5d7ab73259831ed68654b09
aqua <aqua.rock.slug@gmail.com>
2025-04-17 14:07:52 -0500

    trails

--- game.js
+++ game.js
@@ -5,25 +5,27 @@
 function gameInit() {
 	[width, height] = [100, 100];
 	cameraOffset = vec2(0, 0);
-	cameraScale = 2;
+	cameraScale = 3;
 	cameraPos = vec2(width, height).scale(0.5).add(cameraOffset);
 
 	squareUpdate = (square, color) => ({ square, value: { color } });
 
 	grid = Grid(width, height, rgb(0.5, 0.5, 0.5));
 
-	t = new TileInfo(vec2(0, 0), vec2(32, 32), 0);
-	molecule = new Molecule(vec2(0, 0), vec2(10, 10), t, 0);
+	t = new TileInfo(vec2(5, 5), vec2(32, 32), 0);
+	molecule = new Molecule(vec2(20, 20), vec2(10, 10), t, 0);
 }
 function gameStart() {}
 function gameUpdate() {
-	let moleculeTrail = FPO.map({
-		arr: [1, 2, 3, 4, 5], //molecule.squares(),
+	let example = FPO.map({
+		arr: [1, 2, 3, 4, 5],
 		fn: ({ v }) => squareUpdate(v, rgb(1, 0, 0)),
 	});
 
+	let trail = squareUpdate(molecule.center(), rgb(0, 1, 0));
+
 	// combine all squareUpdates into one list and apply them to the grid
-	grid.apply(FPO.flatten({ v: [moleculeTrail] }));
+	grid.apply(FPO.flatten({ v: [example, trail] }));
 }
 function gameUpdatePost() {}
 function gameRender() {
--- molecule.js
+++ molecule.js
@@ -1,8 +1,4 @@
 class Molecule extends EngineObject {
-	constructor(pos, size, tileInfo, angle) {
-		super(pos, size, tileInfo, angle);
-	}
-
 	update() {
 		this.velocity = vec2(1);
 		super.update();
@@ -10,5 +6,20 @@ class Molecule extends EngineObject {
 
 	render() {
 		super.render();
+	}
+
+	// returns the index of the square directly below this molecule
+	center() {
+		for (let col = 0; col < width * height; col += height) {
+			// find the correct column
+			if (Math.round(grid.positions()[col].x) === Math.round(this.pos.x)) {
+				for (let row = 0; row < height; row++) {
+					// find the correct row
+					if (Math.round(grid.positions()[row].y) === Math.round(this.pos.y)) {
+						return col + row;
+					}
+				}
+			}
+		}
 	}
 }

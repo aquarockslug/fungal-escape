609acd7ad255bccb91aa8e1b18650745d92896f7
aqua <aqua.rock.slug@gmail.com>
2025-04-18 16:09:11 -0500

    refactor molecule

--- molecule.js
+++ molecule.js
@@ -10,16 +10,14 @@ class Molecule extends EngineObject {
 
 	// returns the index of the square directly below this molecule
 	center() {
-		for (let col = 0; col < width * height; col += height) {
-			// find the correct column
-			if (Math.round(grid.positions()[col].x) === Math.round(this.pos.x)) {
-				for (let row = 0; row < height; row++) {
-					// find the correct row
-					if (Math.round(grid.positions()[row].y) === Math.round(this.pos.y)) {
-						return col + row;
-					}
-				}
-			}
-		}
+		let findRow = (col) => {
+			for (let row = 0; row < height; row++)
+				if (Math.round(grid.positions()[row].y) === Math.round(this.pos.y))
+					return row;
+		};
+
+		for (let col = 0; col < width * height; col += height)
+			if (Math.round(grid.positions()[col].x) === Math.round(this.pos.x))
+				return col + findRow(col);
 	}
 }

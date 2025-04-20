fa20def022f77ba53fe958dc00985c979622cd5c
aqua <aqua.rock.slug@gmail.com>
2025-04-20 03:02:49 -0500

    added pulse

--- game.js
+++ game.js
@@ -3,29 +3,55 @@
 // and manage the reactions between molecules
 
 function gameInit() {
-	[width, height] = [100, 100];
+	[width, height] = [150, 150];
 	cameraOffset = vec2(0, 0);
-	cameraScale = 3;
+	cameraScale = 4;
 	cameraPos = vec2(width, height).scale(0.5).add(cameraOffset);
 
-	squareUpdate = (square, color) => ({ square, value: { color } });
+	gridUpdate = (square, color) => ({ square, value: { color } });
 
 	grid = Grid(width, height, rgb(0.5, 0.5, 0.5));
 
 	t = new TileInfo(vec2(5, 5), vec2(32, 32), 0);
 	molecule = new Molecule(vec2(20, 20), vec2(10, 10), t, 0);
 }
+// WARN only checks blue
+function findColor(color) {
+	return FPO.filter({
+		arr: FPO.map({
+			arr: grid.values(),
+			fn: ({ i, v }) =>
+				v.color.r === color.r || v.color.g === color.g || v.color.b === color.b
+					? i
+					: -1,
+		}),
+		fn: ({ v }) => v >= 0,
+	});
+}
 function gameStart() {}
 function gameUpdate() {
+	// particle drift
+
+	let blueUpdates = [];
+	let blueParticles = findColor(rgb(0, 0, 1));
+	let blueParticlesOld = FPO.map({
+		arr: blueParticles,
+		fn: ({ v }) => gridUpdate(v, rgb(0.5, 0.5, 0.5)),
+	});
+	let blueParticlesNew = FPO.map({
+		arr: blueParticles,
+		fn: ({ v }) => gridUpdate(grid.neighborsOf(v)[randInt(0, 8)], rgb(0, 0, 1)),
+	});
+	blueUpdates = FPO.flatten({ v: [blueParticlesOld, blueParticlesNew] });
+
 	let example = FPO.map({
-		arr: [1, 2, 3, 4, 5],
-		fn: ({ v }) => squareUpdate(v, rgb(1, 0, 0)),
+		fn: ({ v }) => gridUpdate(v, rgb(1, 0, 0)),
 	});
 
-	let trail = squareUpdate(molecule.center(), rgb(0, 1, 0));
+	let t = trail(molecule);
 
-	// combine all squareUpdates into one list and apply them to the grid
-	grid.apply(FPO.flatten({ v: [example, trail] }));
+	// combine all gridUpdates into one list and apply them to the grid
+	grid.apply(FPO.flatten({ v: [example, t, blueUpdates] }));
 }
 function gameUpdatePost() {}
 function gameRender() {
@@ -34,3 +60,20 @@ function gameRender() {
 		drawRect(grid.positions()[i], vec2(1), grid.values()[i].color);
 }
 function gameRenderPost() {}
+
+function under(molecule) {
+	let center = molecule.center();
+	let squares = grid.neighborsOf(center);
+	squares.push(center);
+	return squares;
+}
+
+function trail(molecule, thickness = 5) {
+	return FPO.map({
+		arr: FPO.filter({
+			arr: under(molecule),
+			fn: ({ v }) => randInt(0, 100) < thickness,
+		}),
+		fn: ({ v }) => gridUpdate(v, rgb(0, 0, 1)),
+	});
+}
--- molecule.js
+++ molecule.js
@@ -1,6 +1,17 @@
 class Molecule extends EngineObject {
+	constructor(pos, size, tileInfo, angle) {
+		super(pos, size, tileInfo, angle);
+		this.direction = vec2(1, 0.2);
+		this.pulse = new Timer(1);
+	}
+
 	update() {
-		this.velocity = vec2(1);
+		this.velocity = this.direction;
+		if (this.pos.x > grid.positions(width * height)) this.direction = vec2(-1);
+		if (this.pulse.elapsed()) {
+			console.log("pulse")
+			this.pulse = new Timer(1);
+		}
 		super.update();
 	}
 
@@ -16,6 +27,7 @@ class Molecule extends EngineObject {
 					return row;
 		};
 
+		// only check the columns and then the rows to avoid checking every square
 		for (let col = 0; col < width * height; col += height)
 			if (Math.round(grid.positions()[col].x) === Math.round(this.pos.x))
 				return col + findRow(col);

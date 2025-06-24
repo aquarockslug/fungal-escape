a45b825117420996df5ebf4c9cc60520ba26a34d
aqua <aqua.rock.slug@gmail.com>
2025-06-23 23:46:48 -0500

    fixed build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-7z u -tzip game assets/**/* javascript/**/* css/**/* index.html
-
+#!/bin/zsh
+# creates a zip file containing all of the files required to play the game on a web browser
+setopt extendedglob; 7z u -tzip game **/^(*.zip|*.md|*.sh|biome.json)

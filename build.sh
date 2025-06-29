0db8560e3aafd658c64ca6d5cdf439147bdf7a8b
aqua <aqua.rock.slug@gmail.com>
2025-06-29 03:25:15 -0500

    added itchio upload to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/zsh
-# creates a zip file containing all of the files required to play the game on a web browser
-setopt extendedglob; 7z u -tzip game **/^(*.zip|*.md|*.sh|biome.json)
+
+# the name of the file
+ARCHIVE_NAME="game"
+
+# create a zip file containing only the files required to play
+setopt extendedglob && 7z u -tzip $ARCHIVE_NAME **/^(*.zip|*.md|*.sh|biome.json) | tail
+
+# upload the zip file to itch.io
+echo && butler push $ARCHIVE_NAME.zip aquarock/spore-shooter:build
